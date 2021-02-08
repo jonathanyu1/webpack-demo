27b8a01899803814ccba2be4b1c025b967cca614
jonathanyu1 <jonathanyusir@gmail.com>
2021-02-07 21:16:21 -0500

    testing webpack and es6 modules

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+function newComponent() {
+  const element = document.createElement('div');
+
+  // use your function
+  element.innerHTML = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(newComponent());
